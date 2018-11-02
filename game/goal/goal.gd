414956497c1cbde3b41a74d476f61e7c2eb4d875
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-02 18:11:30 +0100

    Instance balls and paddles in levels, add goals
    Their position will vary depending on the level.

---
+++ game/goal/goal.gd
@@ -0,0 +1,25 @@
+# Copyright © 2018 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends StaticBody2D
+class_name Goal
+
+export(int, 1, 100) var balls_required = 10
+
+onready var BallCounter = $BallCounter
+
+func _ready() -> void:
+	update_ball_counter(balls_required)
+
+func update_ball_counter(balls) -> void:
+	BallCounter.text = str(balls)
+
+func _draw() -> void:
+	draw_circle(Vector2(), 30.0, Color(0.3, 0.7, 1.0, 1))
+
+func _on_score_area_body_entered(body: PhysicsBody2D):
+	if body is Ball:
+		var ball = body
+		balls_required -= 1
+		update_ball_counter(balls_required)
+		ball.queue_free()
