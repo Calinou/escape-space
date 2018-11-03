8d7dd7327fee868fe6850182b595d3626fedd0fe
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-03 19:30:32 +0100

    Add force fields
    Those block the paddle but let balls pass through,
    and can be of any width.

---
+++ game/force_field/force_field.gd
@@ -0,0 +1,16 @@
+# Copyright © 2018 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+tool
+extends NinePatchRect
+class_name ForceField
+
+# The default width of the force field in pixels
+const DEFAULT_WIDTH = 128
+
+onready var body = $StaticBody2D as StaticBody2D
+
+func _ready() -> void:
+	# Update the body's width to match the size of the NinePatchRect
+	body.position.x = rect_size.x / 2
+	body.scale.x = float(rect_size.x) / DEFAULT_WIDTH
