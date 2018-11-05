2af1670cec6d549075de929ab672f2df583e781a
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-05 14:12:27 +0100

    Add particles and auras to balls to look more like comets
    Claimed balls now look different and sport an orange aura
    instead of a blue aura for unclaimed balls.

--- game/ball/ball.gd
+++ game/ball/ball.gd
@@ -4,8 +4,6 @@
 extends RigidBody2D
 class_name Ball
 
-var motion := Vector2()
-
 # The starting ball speed
 const BASE_SPEED = 275
 
@@ -18,6 +16,11 @@ var speed_factor := 1.01
 # Whether the ball has been touched by a paddle or not
 var claimed := false
 
+onready var sprite = $Sprite as Sprite
+onready var particles = $CPUParticles2D as CPUParticles2D
+
+var motion := Vector2()
+
 func _draw() -> void:
 	draw_circle(Vector2(), 10.0, Color(0.9, 0.9, 0.9, 1))
 
@@ -26,8 +29,14 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 
 	for body in get_colliding_bodies():
 		if body is Paddle:
-			claimed = true
+			claim()
 
 		if not claimed:
 			# Destroy unclaimed balls on their first collision
 			queue_free()
+
+# Called when the ball is claimed (i.e. touched by a paddle)
+func claim():
+	claimed = true
+	sprite.modulate = Color(1.0, 0.65, 0.25, 0.7)
+	particles.modulate = Color(1.0, 0.65, 0.25, 1.0)
