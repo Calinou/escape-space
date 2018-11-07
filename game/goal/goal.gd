0683c6e28f7baf255d1343617e7409f911226dca
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-07 15:20:39 +0100

    Add an animation when a goal is closed, make it not accept balls after

--- game/goal/goal.gd
+++ game/goal/goal.gd
@@ -6,6 +6,7 @@ class_name Goal
 
 export(int, 1, 100) var balls_required = 10
 
+onready var animation_player = $AnimationPlayer as AnimationPlayer
 onready var ball_counter = $Panel/BallCounter as Label
 
 func _ready() -> void:
@@ -17,9 +18,13 @@ func update_ball_counter(balls: int) -> void:
 func _draw() -> void:
 	draw_circle(Vector2(), 30.0, Color(0.3, 0.7, 1.0, 1))
 
+# The area's collision shape is disabled when the number of balls required
+# reaches 0, so it won't be called anymore after the goal is complete
 func _on_score_area_body_entered(body: PhysicsBody2D):
 	if body is Ball:
-		var ball = body
+		body.queue_free()
 		balls_required -= 1
 		update_ball_counter(balls_required)
-		ball.queue_free()
+
+		if balls_required == 0:
+			animation_player.play("close")
