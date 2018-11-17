06b81c7531fa5c7f141cc212134e01022a3181d9
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-17 18:59:35 +0100

    Toggle fullscreen when pressing F11 or Alt + Enter

--- autoload/settings.gd
+++ autoload/settings.gd
@@ -11,6 +11,16 @@ func _ready() -> void:
 	# Loads existing configuration (if any) for use anywhere
 	file.load(CONFIG_PATH)
 
+	OS.window_fullscreen = bool(file.get_value("video", "fullscreen", false))
+
+func _input(event: InputEvent) -> void:
+	# Fullscreen toggle
+	# This can be done from anywhere, so it should be in a singleton
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+		file.set_value("video", "fullscreen", OS.window_fullscreen)
+		save()
+
 # Saves the configuration file with a pre-defined path.
 # This method should be used over `Settings.file.save(path)`
 # unless a custom path needs to be specified.
