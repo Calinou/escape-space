3bd62aed97357cbe09f221901afa38957d83b843
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-24 22:19:32 +0100

    Add transitions between menus
    - Make it possible for child GUIs to listen to transition state
      for use with `yield()`
    - Make the initial "fade in" animation slightly slower and use easing
    - Use Process instead of Idle animation mode to prevent floating-point
      precision errors from shifting the menu when clicking rapidly

--- menu/main.gd
+++ menu/main.gd
@@ -6,12 +6,16 @@ extends Control
 signal menu_changed
 
 func _on_play_pressed() -> void:
+	emit_signal("menu_changed")
+	yield($"/root/Menu", "transition_finished")
 	get_tree().change_scene("res://game/game.tscn")
 
 func _on_options_pressed() -> void:
 	emit_signal("menu_changed", $"/root/Menu/Options")
 
 func _on_quit_pressed() -> void:
+	emit_signal("menu_changed")
+	yield($"/root/Menu", "transition_finished")
 	get_tree().quit()
 
 func _on_credits_pressed() -> void:
--- menu/menu.gd
+++ menu/menu.gd
@@ -11,6 +11,8 @@ onready var preloader := $ResourcePreloader as ResourcePreloader
 onready var hover_sound := preloader.get_resource("hover") as AudioStream
 onready var click_sound := preloader.get_resource("click") as AudioStream
 
+signal transition_finished
+
 func _ready() -> void:
 	animation_player.play("fade_in")
 	Music.play_song("menu/menu")
@@ -32,7 +34,20 @@ func _control_pressed(control: Control) -> void:
 	Sound.play(Sound.Type.NON_POSITIONAL, self, click_sound, -2.0)
 
 # Called when a child GUI sets the currently-viewed GUI.
-func _on_menu_changed(new_menu: Control) -> void:
-	current_menu.visible = false
-	new_menu.visible = true
-	current_menu = new_menu
+# If no GUI is set, then only the animation will be played;
+# the child GUI will have to handle the action by itself.
+func _on_menu_changed(new_menu: Control = null) -> void:
+	animation_player.play("change_menu")
+
+	# Change the GUI when the animation is halfway done
+	yield(
+			get_tree().create_timer(animation_player.current_animation_length / 2),
+			"timeout"
+	)
+
+	if new_menu:
+		current_menu.visible = false
+		new_menu.visible = true
+		current_menu = new_menu
+
+	emit_signal("transition_finished")
