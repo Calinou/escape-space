d9ab1aa54ee65ace94903df21a337a057734c05b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-11-25 21:45:29 +0100

    Add a 10-minute bias when considering the date of a new version

--- menu/update_checker.gd
+++ menu/update_checker.gd
@@ -8,10 +8,16 @@ onready var version_label := $Version as Label
 onready var update_button := $Update as Button
 onready var http_request := $HTTPRequest as HTTPRequest
 
-# Only update once every N seconds at most
+# Only update once every UPDATE_THROTTLE seconds at most
 # The GitHub releases API only allows 60 unauthenticated requests per hour,
 # so this value should be kept relatively high
 const UPDATE_THROTTLE = 600
+
+# If an update is less than VERSION_DATE_BIAS seconds newer than the
+# current version, then it won't be considered as a new version.
+# This is done to account for deployment taking some time after the
+# project is built
+const VERSION_DATE_BIAS = 600
 
 func _ready() -> void:
 	version_label.text = ProjectSettings.get("application/config/version")
@@ -23,8 +29,10 @@ func _ready() -> void:
 # TODO: Check for stable releases only (depending on user preference)
 func check_for_updates() -> void:
 	if OS.get_unix_time() < Settings.cache.get_value("updates", "last_check", 0) + UPDATE_THROTTLE:
-		print("INFO: Not checking for updates since they were already recently checked.")
+		print("INFO: Skipping update check since updates were recently checked.")
 		return
+	else:
+		print("INFO: Checking for updates…")
 
 	http_request.request(
 			"https://api.github.com/repos/{user}/{repository}/releases".format({
@@ -38,6 +46,8 @@ func _on_http_request_completed(result: int, response_code: int, headers: PoolSt
 		# The request failed for any reason, abort
 		print("ERROR: Could not check for updates due to a network error.")
 		return
+	else:
+		print("INFO: Update check successful.")
 
 	# Store timestamp of last successful update check (used for throttling)
 	Settings.cache.set_value("updates", "last_check", OS.get_unix_time())
@@ -46,7 +56,7 @@ func _on_http_request_completed(result: int, response_code: int, headers: PoolSt
 	var json = parse_json(body.get_string_from_utf8())
 	var latest_unix = OS.get_unix_time_from_datetime(parse_date(json[0].created_at))
 
-	if latest_unix > ProjectSettings.get("application/config/version_date"):
+	if latest_unix > ProjectSettings.get("application/config/version_date") + VERSION_DATE_BIAS:
 		animation_player.play("fade_in")
 
 		# Set the URL that will be opened when the button is pressed
