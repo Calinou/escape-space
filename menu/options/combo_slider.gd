5f7c7f3effc48e01001c9375afe292d968116ebb
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-02-19 22:22:39 +0100

    Make sliders resettable to their default value by right-clicking

--- menu/options/combo_slider.gd
+++ menu/options/combo_slider.gd
@@ -6,8 +6,19 @@ extends HBoxContainer
 onready var spin_box := $SpinBox as SpinBox
 onready var slider := $HSlider as Slider
 
+# The slider's default value.
+# Used to reset the slider to its default value when right-clicked
+onready var default := slider.value
+
 func _on_slider_value_changed(value: float) -> void:
 	spin_box.value = value
 
 func _on_spin_box_value_changed(value: float) -> void:
 	slider.value = value
+
+func _on_slider_gui_input(event: InputEvent) -> void:
+	if (
+		event is InputEventMouseButton and
+		(event as InputEventMouseButton).button_index == BUTTON_RIGHT
+	):
+		slider.value = default
