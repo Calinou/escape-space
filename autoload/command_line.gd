7e13183a20859321585b79a2fc98f2ff03659a5b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-06-05 18:17:36 +0200

    Add missing `command_line.gd` autoload script

---
+++ autoload/command_line.gd
@@ -0,0 +1,13 @@
+# Copyright © 2018-2019 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+
+extends Node
+
+var arguments = {}
+
+func _ready():
+	for argument in OS.get_cmdline_args():
+		# Parse valid command-line arguments of the form `--key=value` into a dictionary
+		if argument.find("=") > -1:
+			var key_value = argument.split("=")
+			arguments[key_value[0].lstrip("--")] = key_value[1]
